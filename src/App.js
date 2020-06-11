a1f6622bec1f3097320b18abe8976b0c46a2dd3d
Ruan Ramos <ruan.ramos28@gmail.com>
2020-06-11 17:02:16 -0300

    Final guided project, without css (lost it, not on purpose)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,78 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import CurrencyRow from './CurrencyRow'
 
-class App extends Component {
-  render() {
-    return (
-      <h1>Hello World!!!</h1>
-    );
+const BASE_URL = 'https://api.exchangeratesapi.io/latest'
+
+function App() {
+
+  const [currencyOptions, setCurrencyOptions] = useState([])
+  const [fromCurrency, setFromCurrency] = useState()
+  const [toCurrency, setToCurrency] = useState()
+  const [exchangeRate, setExchangeRate] = useState()
+  const [amount, setAmount] = useState(1)
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true)
+  
+
+  let toAmount, fromAmount
+  if (amountInFromCurrency) {
+    fromAmount = amount
+    toAmount = amount * exchangeRate
+  } else {
+    toAmount = amount
+    fromAmount = amount / exchangeRate
   }
+
+  useEffect(() => {
+    fetch(BASE_URL)
+    .then(res => res.json())
+    .then(data => {
+      const firstCurrency = Object.keys(data.rates)[0]
+      console.log(data.rates)
+      setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+      setFromCurrency(data.base)
+      setToCurrency(firstCurrency)
+      setExchangeRate(data.rates[firstCurrency])
+    })
+  }, [])
+
+  useEffect(() => {
+    if (fromCurrency != null && toCurrency != null) {
+      fetch(`${BASE_URL}?base=${fromCurrency}&symbols=${toCurrency}`)
+        .then(res => res.json())
+        .then(data => setExchangeRate(data.rates[toCurrency]))
+    }}, [toCurrency, fromCurrency])
+
+  function handleFromAmountChange(e) {
+    setAmount(e.target.value)
+    setAmountInFromCurrency(true)
+  }
+
+  function handleToAmountChange(e) {
+    setAmount(e.target.value)
+    setAmountInFromCurrency(false)
+  }
+
+  return (
+    <React.Fragment>
+      <h1>Currency Converter</h1>
+      <CurrencyRow 
+        currencyOptions={currencyOptions} 
+        selectedCurrency={fromCurrency}
+        onChangeCurrency={e => setFromCurrency(e.target.value)}
+        amount={fromAmount}
+        onChangeAmount={handleFromAmountChange}
+      />
+      <div className="equals">=</div>
+      <CurrencyRow 
+        currencyOptions={currencyOptions}
+        selectedCurrency={toCurrency}
+        onChangeCurrency={e => setToCurrency(e.target.value)}
+        amount={toAmount}
+        onChangeAmount={handleToAmountChange}
+      />
+    </React.Fragment>
+  );
 }
 
 export default App;
